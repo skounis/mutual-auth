3d39dbf8b2d6d61140342dfaa51d6aa2fc24f133
Stavros Kounis <skounis@gmail.com>
2021-11-09 18:21:48 +0100

    feat(api) Scaffold endpoints with mock data.

--- server.js
+++ server.js
@@ -14,11 +14,35 @@ const options = {
   // will make it to the specified route specified
   rejectUnauthorized: true
 };
-
+// Load mock data
+const rawdata = fs.readFileSync(__dirname + "/data/" + "data.json", 'utf8');
+const data = JSON.parse(rawdata);
 const app = express();
 app.get('/', function(req,res) {
   res.send('"OK!');
 });
+
+app.get('/', function (req, res) {
+  // fs.readFile( __dirname + "/data/" + "crl.json", 'utf8', function (err, data) {
+  //    console.log( data );
+  //    res.end( data );
+  // });
+  res.end(JSON.stringify(data.crl, null, 2));
+})
+
+app.get('/feds', function (req, res) {
+  res.end(JSON.stringify(data.federators, null, 2));
+})
+
+app.get('/crl', function (req, res) {
+  res.end(JSON.stringify(data.crl, null, 2));
+})
+
+
+app.get('/batches/:id', function (req, res) {
+  res.end(JSON.stringify(data.batches[req.params.id], null, 2));
+})
+
 
 https
   .createServer(options, app)
